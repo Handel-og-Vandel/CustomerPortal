3d841bbdd4172bb0c093d30bcd8584658985ee34
Henrik L. Jensen <hnrk@eaaa.dk>
2025-03-07 13:58:01 +0100

    Update to model and order form

--- CustomerPortalFrontend/Models/Order.cs
+++ CustomerPortalFrontend/Models/Order.cs
@@ -1,9 +1,82 @@
-namespace Models
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Models;
+
+public class Order
 {
-    public class Order
-    {
-        public int OrderId { get; set; }
-        public DateTime OrderDate { get; set; }
-        public required string OrderStatus { get; set; }
-    }
+    [Required]
+    public Guid OrderId { get; set; }
+
+    [Required]
+    public Guid CustomerId { get; set; }
+
+    [Required]
+    public DateTime OrderDate { get; set; }
+
+    [Required]
+    [MinLength(0)]
+    public required List<OrderItem> Items { get; set; }
+
+    [Required]
+    public required Address ShippingAddress { get; set; }
+
+    [Required]
+    [JsonConverter(typeof(JsonStringEnumConverter<PaymentMethod>))]
+    public PaymentMethod PaymentMethod { get; set; }
+
+    [Required]
+    [Range(0, double.MaxValue)]
+    public double TotalAmount { get; set; }
+
+    [Required]
+    [JsonConverter(typeof(JsonStringEnumConverter<OrderStatus>))]
+    public OrderStatus OrderStatus { get; set; }
+}
+
+public class OrderItem
+{
+    [Required]
+    public required Guid ProductId { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int Quantity { get; set; }
+
+    [Required]
+    [Range(0, double.MaxValue)]
+    public double Price { get; set; }
+}
+
+public class Address
+{
+    [Required]
+    public required string Street { get; set; }
+
+    [Required]
+    public required string City { get; set; }
+
+    public string? State { get; set; } // State is optional, so no [Required]
+
+    [Required]
+    public required string PostalCode { get; set; }
+
+    [Required]
+    public required string Country { get; set; }
+}
+
+public enum PaymentMethod
+{
+    credit_card,
+    paypal,
+    bank_transfer
+}
+
+public enum OrderStatus
+{
+    pending,
+    processing,
+    shipped,
+    delivered,
+    cancelled
 }
